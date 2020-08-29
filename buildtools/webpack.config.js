32a0e7bc23fa9f5abff388748822c1a6cc813018
puzzledmonkey <chris@oedo.net>
2020-08-29 17:32:56 +0900

    small dist

--- buildtools/webpack.config.js
+++ buildtools/webpack.config.js
@@ -27,6 +27,11 @@ module.exports = {
   externals: [
     {
       Phaser: { commonjs: 'Phaser', commonjs2: 'Phaser', root: 'Phaser' },
+      '@jollywise/jollygoodgame': {
+        commonjs: 'jollygoodgame',
+        commonjs2: 'jollygoodgame',
+        root: 'jollygoodgame',
+      },
       webfontloader: {
         commonjs: 'webfontloader',
         commonjs2: 'webfontloader',
