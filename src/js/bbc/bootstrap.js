a473ab9e91e3911d119e949f7d03a4dedf07b6b2
Stephen Hamilton <stephenhamilton@mac.com>
2020-08-31 11:14:48 +0100

    Adjusts bootstrapBBC : we only append css on BBC env
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

--- src/js/bbc/bootstrap.js
+++ src/js/bbc/bootstrap.js
@@ -1,12 +1,16 @@
-export const bootstrapBBC = (gameDir) => {
+export const bootstrapBBC = (isBBC = false, gameDir) => {
   const promise = new Promise((resolve) => {
-      // Add css to html head
+    if (isBBC) {
+      // In BBC env : Add css to html head
       const ss = document.createElement('link');
       ss.type = 'text/css';
       ss.rel = 'stylesheet';
       ss.href = gameDir + 'css/main.css';
       document.getElementsByTagName('head')[0].appendChild(ss);
       resolve({ success: true });
+    } else {
+      resolve({ success: true });
+    }
   });
   return promise;
 };
