480fd8f7d308e60ad784e5057538584fdb57aa77
Stephen Hamilton <stephenhamilton@mac.com>
2020-09-01 10:25:05 +0100

    Adds typings
    Signed-off-by: Stephen Hamilton <stephenhamilton@mac.com>

---
+++ typings/bbc.d.ts
@@ -0,0 +1,1 @@
+export type BBC_STATS_BASE = {};
---
+++ typings/index.d.ts
@@ -0,0 +1,5 @@
+/// <reference types="@jollywise/jollygoodgame" />
+
+export as namespace JGG_BBC;
+export * from './AppBBC';
+export * from './bbc';
