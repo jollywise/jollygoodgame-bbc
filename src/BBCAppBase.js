e1596008b86e40fd8e4c0791dc9a25d7fddd14fd
puzzledmonkey <chris@oedo.net>
2021-03-13 20:53:06 +0900

    fix AppBaseBBC/BBCAppBase inconsistency

--- src/BBCAppBase.js
+++ src/BBCAppBase.js
@@ -1,3 +1,20 @@
-import { AppBase } from '@jollywise/jollygoodgame';
+import { AppBase, StorageGame } from '@jollywise/jollygoodgame';
+import { TrackingPlugin } from './bbc/plugins/TrackingPlugin';
+import { StoragePlugin } from './bbc/plugins/StoragePlugin';
 
-export default class BBCAppBase extends AppBase {}
+export default class BBCAppBase extends AppBase {
+  constructor(opts) {
+    super(opts);
+
+    // base url
+    this.gmi = opts.gmi;
+
+    // BBC Saves
+    const storage = new StorageGame('');
+    storage.plugin = new StoragePlugin(opts.gmi);
+    this.saves.storage = storage;
+
+    // BBC Tracking
+    this.tracking.plugin = new TrackingPlugin(opts.gmi, true);
+  }
+}
