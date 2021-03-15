1046ae42fefcafa3e344b5d3f36b25b4992175e2
puzzledmonkey <chris@oedo.net>
2021-03-15 18:07:22 +0900

    add gmi to config

--- src/config.js
+++ src/config.js
@@ -1,8 +1,40 @@
-// eslint-disable-next-line no-unused-vars
 import { getConfigBase } from '@jollywise/jollygoodgame';
 
 export const getConfigBBC = (opts) => {
   const conf = getConfigBase(opts);
-
+  if (opts.gmi && opts.gmi.gameContainerId) {
+    opts.parent = opts.gmi.gameContainerId;
+  }
   return conf;
+};
+
+export const getGmiConfig = () => {
+  gmiConfig = {
+    pages: [
+      {
+        title: 'Game Settings',
+        settings: [
+          {
+            key: 'audio',
+            type: 'toggle',
+            title: 'Audio',
+            description: 'Turn off/on sound and music',
+          },
+          {
+            key: 'motion',
+            type: 'toggle',
+            title: 'Motion',
+            description: 'Turn off/on motion',
+          },
+          {
+            key: 'subtitles',
+            type: 'toggle',
+            title: 'Subtitles',
+            description: 'Turn off/on subtitles',
+            value: true,
+          },
+        ],
+      },
+    ],
+  };
 };
