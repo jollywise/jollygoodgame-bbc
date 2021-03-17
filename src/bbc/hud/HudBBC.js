e47a66104e7ac8c6417fda1a8dc39694dc791891
Paul Garrett <15093657+Jiminibob@users.noreply.github.com>
2021-03-17 20:58:50 +0000

    Enable 'settings icons' in hud

--- src/bbc/hud/HudBBC.js
+++ src/bbc/hud/HudBBC.js
@@ -1,8 +1,18 @@
-import { Hud } from '@jollywise/jollygoodgame';
+import { Hud, SETTINGS_EVENTS } from '@jollywise/jollygoodgame';
 import { ButtonBBC } from '@jollywise/jollygoodgame-bbc/src/bbc/hud/ButtonBBC';
 import { ButtonSoundBBC } from '@jollywise/jollygoodgame-bbc/src/bbc/hud/ButtonSoundBBC';
 
 export default class HudBBC extends Hud {
+
+  constructor(){
+    super({ key: 'Hud', active: true });
+  }
+
+  create(){
+    super.create();    
+    this.game.settings.on(SETTINGS_EVENTS.CHANGED, this.setSettingsIcons, this)
+  }
+
   createStateButton(config, overwrite = false) {
     // button already exists
     if (this._stateButtons[config.id]) {
@@ -34,5 +44,58 @@ export default class HudBBC extends Hud {
 
     // register button
     this._stateButtons[config.id] = btn;
+  }
+
+  setState(hudid, save = true){
+    super.setState(hudid, save = true);
+    this.setSettingsIcons();
+  }
+
+  updatePosition(){
+    super.updatePosition();
+    this.setSettingsIcons()
+  }
+
+
+  setSettingsIcons(){
+    if(!this.settingsIcons){
+      const icons= this._hudConfig.settings_icons
+      if(icons){
+        const keys = Object.keys(icons);
+        this.settingsIcons = keys.map((key) => {
+          const data =icons[key];
+          const icon = this.add.image(0,0,data.sprite||'buttons',data.id).setOrigin(0, 0.5);
+          icon.setData('config', data);
+          return icon;
+        });
+      }
+    }
+
+    const group = this.getButtonGroup('topright')
+    let width = group ? group.groupwidth + 40 : 0;
+    this.settingsIcons.forEach((icon) => {
+
+      const {hide_if_button ,hide_if_setting} = icon.getData('config');
+      if(hide_if_button){
+        const hide = hide_if_button.find((a) => this._stateButtons[a]);
+        if(hide){
+          icon.visible = false;
+          return;
+        }
+      } 
+       if(hide_if_setting){
+        const hide = hide_if_setting.find((a) => this.game.settings.getSetting(a) );
+        if(hide){
+          icon.visible = false;
+          return;
+        }
+      }
+      
+      icon.visible = true;
+      icon.y = this.game.viewportController.topPadded + 25;
+      icon.x = this.game.viewportController.rightPadded - width - icon.displayWidth;
+      width += icon.displayWidth + 20;
+    })
+
   }
 }
