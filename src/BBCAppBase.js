cacf921f61f22411479b9360a4627e0c5a3dfe4a
Paul Garrett <15093657+Jiminibob@users.noreply.github.com>
2021-03-24 20:09:39 +0000

    Update BBCAppBase.js

--- src/BBCAppBase.js
+++ src/BBCAppBase.js
@@ -3,5 +3,10 @@ import { AppBase } from '@jollywise/jollygoodgame';
 export class BBCAppBase extends AppBase {
   constructor(opts) {
     super(opts);
+    this._gmi = opts.gmi;
+  }
+
+  get gmi() {
+    return this._gmi;
   }
 }
