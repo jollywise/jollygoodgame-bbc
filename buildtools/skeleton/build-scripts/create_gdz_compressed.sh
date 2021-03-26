cb149f545e483c0553016a150b6b690628adbd9b
puzzledmonkey <chris@oedo.net>
2021-03-26 14:43:07 +0900

    add build-scripts to skeleton

---
+++ buildtools/skeleton/build-scripts/create_gdz_compressed.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+set -e
+#   Use this to create gdz files for local testing.
+
+#   Give the scripts execute permissions if you get errors
+#   chmod +x create_gdz_compressed.sh
+#   chmod +x create_gdz_uncompressed.sh
+
+#   npm run-script bbc : generate the BBC output files via webpack build
+#   bash create_gdz.sh : generate the gdz (zip essentially)
+#   Connect android device to computer via USB
+#   In chrome : chrome://inspect/#devices (make sure usb debugging enabled on android device)
+
+mkdir -p gdz
+cd 'output'
+zip -r ../gdz/project.gdz -Z store . --exclude=*.sh* --exclude=*.svn* --exclude=*.git* --exclude=*.DS_Store*
