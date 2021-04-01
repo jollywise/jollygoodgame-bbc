865a2049dc530b42b170c391511c02fd1c256f3c
puzzledmonkey <chris@oedo.net>
2021-04-01 19:59:12 +0900

    make gmi settings configurable

--- src/config.js
+++ src/config.js
@@ -20,32 +20,38 @@ export const getConfigBBC = (opts = {}) => {
   return conf;
 };
 
-export const getConfigGMI = () => {
+export const getConfigGMI = ({ audio = true, motion = true, subtitles = true } = {}) => {
+  const settings = [];
+  if (audio) {
+    settings.push({
+      key: 'audio',
+      type: 'toggle',
+      title: 'Audio',
+      description: 'Turn off/on sound and music',
+    });
+  }
+  if (motion) {
+    settings.push({
+      key: 'motion',
+      type: 'toggle',
+      title: 'Motion',
+      description: 'Turn off/on motion',
+    });
+  }
+  if (subtitles) {
+    settings.push({
+      key: 'subtitles',
+      type: 'toggle',
+      title: 'Subtitles',
+      description: 'Turn off/on subtitles',
+      value: true,
+    });
+  }
   return {
     pages: [
       {
         title: 'Game Settings',
-        settings: [
-          {
-            key: 'audio',
-            type: 'toggle',
-            title: 'Audio',
-            description: 'Turn off/on sound and music',
-          },
-          // {
-          //   key: 'motion',
-          //   type: 'toggle',
-          //   title: 'Motion',
-          //   description: 'Turn off/on motion',
-          // },
-          {
-            key: 'subtitles',
-            type: 'toggle',
-            title: 'Subtitles',
-            description: 'Turn off/on subtitles',
-            value: true,
-          },
-        ],
+        settings: settings,
       },
     ],
   };
